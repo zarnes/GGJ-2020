168124b92af2491d8d6f958fccd6ade6f0945830
zarnes <zarnes@live.com>
2020-02-01 19:28:38 +0100

    Added graphs

---
+++ Assets/Objects/Board/Board.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Transform CellsParent;
+    public GameObject CellPrefab;
+
+    [Space]
+    public List<GridSystem> Grids;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(GridSystem grid in Grids)
+        {
+            for (int x = 0; x < grid.GridSize.x; ++x)
+            {
+                for (int y = 0; y < grid.GridSize.y; ++y)
+                {
+                    Instantiate(CellPrefab, grid.GridToWorld(new Vector2Int(x, y)), Quaternion.identity, CellsParent);
+                }
+            }
+        }
+    }
+}

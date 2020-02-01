5ec97f9963cdd87c8ba6cfa4d78fe981bf3a8621
Maupied <b.maupied@student.isartdigital.com>
2020-02-01 02:05:51 +0100

    Implement sample object, add mirror class for scriptable objects. Create function to spawn object with mouse click.

--- Assets/Objects/Grid Object/GridObject.cs
+++ Assets/Objects/Grid Object/GridObject.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridObject : ScriptableObject
+public class GridObject : MonoBehaviour
 {
     public List<Vector2Int> CoordinatesUsed;
     public Vector2Int Position;
+
+    public void InitializeFromDataFile(GridObjectData data)
+    {
+        CoordinatesUsed = data.CoordinatesUsed;
+    }
+
+
 }
--- Assets/Objects/Object Factory/ObjectFactory.cs
+++ Assets/Objects/Object Factory/ObjectFactory.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ObjectFactory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // TODO: remove this logic when other components are setup
+    [SerializeField]
+    private GameObject SampleObject;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // TODO: remove this logic when other components are setup
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                GenerateObject(hit.point);
+            }
+                
+        }
+    }
+
+    void GenerateObject(Vector3 objWorldPosition)
+    {
+        GridSystem gSysteme;
+        Vector2Int gPos;
+
+        if (GridManager.Instance.GetGridCoords(objWorldPosition, out gSysteme, out gPos))
+        {
+            Vector3 worldGridCellPos = gSysteme.GridToWorld(gPos);
+            Quaternion sampleRotation = Quaternion.Euler(-90f, 0f, 0f);
+            GameObject.Instantiate(SampleObject, worldGridCellPos, sampleRotation);
+
+            print("Grid type : " + gSysteme.Inventory.Type);
+            print("gPos : " + gPos);
+        }
     }
 }

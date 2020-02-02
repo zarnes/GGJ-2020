87c00d2d845ffabf7025abb9d902c98a72044d9a
zarnes <zarnes@live.com>
2020-02-02 10:28:51 +0100

    Fixes

--- Assets/Objects/Grid Object/GridObject.cs
+++ Assets/Objects/Grid Object/GridObject.cs
@@ -107,6 +107,7 @@ public class GridObject : MonoBehaviour
         Vector3 mosPos = Input.mousePosition;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mosPos);
         worldPos.z = 0f;
+        worldPos -= _offset;
 
         transform.position = worldPos;
 
@@ -122,7 +123,7 @@ public class GridObject : MonoBehaviour
         GridSystem gSystem;
         Vector2Int gCoords;
 
-        if (GridManager.Instance.GetGridCoords(transform.position, out gSystem, out gCoords) && gSystem.Inventory.Type != GridInventory.GridType.Input)
+        if (GridManager.Instance.GetGridCoords(transform.position + _offset, out gSystem, out gCoords) && gSystem.Inventory.Type != GridInventory.GridType.Input)
         {
             Vector3 worldPos = gSystem.GridToWorld(gCoords);
             transform.position = gSystem.Inventory.EndMove(gCoords) ? worldPos - _offset : initialDragPosition;
